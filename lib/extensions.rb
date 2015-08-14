46cada3d6299d67fe97197af534ac543eb623cd2
jfcmacro <jfcmacro@gmail.com>
2015-08-14 14:37:49 -0500

    whiplash has a working version with messages and urls

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -1,107 +1,116 @@
 require 'singleton'
 
-class LangDescr
+LangDescr = Class.new do 
   attr_accessor :name, :ext
   attr_writer :genComment
 
   def prodComment(str)
-    genCommment str
+    @genComment.call(str)
   end
 end
 
-def hashComment(str)
-  return "# " + str + "\n"
+$hashComment = Proc.new do |str|
+  "# " + str + "\n"
 end
 
-def dashComment(str)
-  return "-- " + str + "\n"
+$dashComment = Proc.new do |str|
+  "-- " + str + "\n"
 end
 
-def backComment(str)
-  return "// " + str + "\n"
+$backComment = Proc.new do |str|
+  "// " + str + "\n"
 end
 
-def backaAstComment(str)
-  return "/* " + str + " */"
+$backAstComment = Proc.new do |str|
+  "/* " + str + " */"
 end
 
-def smlComment(str)
-  return "(* " + str + " *)" + "\n"
+$smlComment = Proc.new do |str|
+  "(* " + str + " *)" + "\n"
 end
 
-
-class LangExtensions
+LangExtensions = Class.new do
   include Singleton
+
   def initialize()
-    rubyLang = LangDescr.new do |r|
+    rubyLang = LangDescr.new.tap do |r|
       r.name = "ruby"
       r.ext = [".rb"]
-      r.genComment = hashComment
+      r.genComment = $hashComment
     end
 
-    haskellLang = LangDescr.new do |h|
+    haskellLang = LangDescr.new.tap do |h|
       h.name = "Haskell"
       h.ext = [".hs", ".lhs"]
-      h.genComment = dashComment
+      h.genComment = $dashComment
     end
 
-    cLang = LangDescr.new do |c|
+    cLang = LangDescr.new.tap do |c|
       c.name = "c"
       c.ext = [".c"]
-      c.genComment = backComment
+      c.genComment = $backComment
     end
 
-    cppLang = LangDescr.new do |cpp|
+    cppLang = LangDescr.new.tap do |cpp|
       cpp.name = "c++"
       cpp.ext = [".cc", ".cpp", ".cxx"]
-      cpp.genComment = backComment
+      cpp.genComment = $backComment
     end
 
-    prologLang = LangDescr.new do |pl|
+    prologLang = LangDescr.new.tap do |pl|
       pl.name = "prolog"
       pl.ext = [".pl"]
-      pl.genComment = backAstComment
+      pl.genComment = $backAstComment
     end
 
-    smlLang = LangDescr.new do |sml|
+    smlLang = LangDescr.new.tap do |sml|
       sml.name = "Standard sml"
       sml.ext = [".sml"]
-      sml.genComment = smlComment
+      sml.genComment = $smlComment
     end
 
-    javaLang = LangDescr.new do |j|
+    javaLang = LangDescr.new.tap do |j|
       j.name = "Java"
       j.ext  = [".java"]
-      j.genComment = backComment
+      j.genComment = $backComment
     end
 
-    eweLang = LangDescr.new do |e|
+    eweLang = LangDescr.new.tap do |e|
       e.name = "ewe"
       e.ext  = [".ewe"]
-      e.genComment = hashComment
+      e.genComment = $hashComment
     end
 
-    bashLang = LangDescr.new do |b|
+    bashLang = LangDescr.new.tap do |b|
       b.name = "bash"
       b.ext  = [".ewe"]
-      b.genComment = hashComment
+      b.genComment = $hashComment
     end
 
-    @filexts = { (rubyLang.ext.[] 0)    => rubyLang
-               }
-    #              haskellLang.ext[0] => haskellLang,
-    #              haskellLang.ext[1] => haskellLang,
-    #              cLang.ext[0]       => cLang,
-    #              cppLang.ext[0]     => cppLang,
-    #              cppLang.ext[1]     => cppLang,
-    #              prologLang.ext[0]  => prologLang,
-    #              smlLang.ext[0]     => smlLang,
-    #              bashLang.ext[0]    => bashLang,
-    #              eweLang.ext[0]     => eweLang,
-    #              javaLang.ext[0]    => javaLang
-    #          }
+    # puts "Lenguaje: #{rubyLang.ext[0]} objeto: #{rubyLang}"
+
+    @filexts = { rubyLang.ext[0]    => rubyLang,
+                 haskellLang.ext[0] => haskellLang,
+                 haskellLang.ext[1] => haskellLang,
+                 cLang.ext[0]       => cLang,
+                 cppLang.ext[0]     => cppLang,
+                 cppLang.ext[1]     => cppLang,
+                 cppLang.ext[2]     => cppLang,
+                 prologLang.ext[0]  => prologLang,
+                 smlLang.ext[0]     => smlLang,
+                 bashLang.ext[0]    => bashLang,
+                 eweLang.ext[0]     => eweLang,
+                 javaLang.ext[0]    => javaLang
+             }
   end
   def getLangDesc(ext)
     @filexts[ext]
   end
 end
+
+# Testing
+# le = LangExtensions.instance
+# lang = le.getLangDesc('.hs')
+# comm = lang.prodComment('Hola mundo')
+#
+# puts "extensions"
